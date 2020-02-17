6d4711aab0d8bcc1d87b938e954f3631ed7d2af1
Mateus Sobrinho <mateussobrinho@vhsolucoes.com.br>
2020-02-17 12:09:25 -0300

    fix endereco pagina de contato

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -85,7 +85,7 @@ const Contact = () => {
             <div className="separator">
               <h4 className="orange-color">Onde estamos</h4>
               <p className="white-color">
-                Rua Nossa Senhora do Rosário - 480 Sala 01 e 02 | Palhoça - SC
+                Rua Nossa Senhora do Rosário - 480 Sala 02 | Palhoça - SC
                 CEP 88138-800
               </p>
             </div>
