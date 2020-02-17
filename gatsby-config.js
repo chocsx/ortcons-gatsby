6654e610b16164877b994f4759eaac5e72d3f0ac
Mateus Sobrinho <mateussobrinho@vhsolucoes.com.br>
2020-02-17 15:06:19 -0300

    Add PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,22 @@ module.exports = {
         showSpinner: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Ortcons Incorporadora",
+        short_name: "Ortcons",
+        start_url: "/",
+        background_color: "#F6A501",
+        theme_color: "#F6A501",
+        display: "standalone",
+        icon: "src/assets/imgs/logo-min.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      }
+    },
+    'gatsby-plugin-offline'
   ],
 }
